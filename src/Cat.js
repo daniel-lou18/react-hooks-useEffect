8c11cec42b7a3a92b8428b0177719c8628242078
daniel <102236708+daniel-lou18@users.noreply.github.com>
2023-05-02 10:06:28 +0000

    Update package-lock.json and Cat.js

--- src/Cat.js
+++ src/Cat.js
@@ -1,49 +1,86 @@
-import { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
-import COLORS from './data/colors.json';
-import VALID_STATUS_CODES from './data/validStatusCodes.json';
+import COLORS from "./data/colors.json";
+import VALID_STATUS_CODES from "./data/validStatusCodes.json";
 
 const Cat = () => {
   const history = useHistory();
   const [colorIdx, setColorIdx] = useState(0);
   const [delayChange, setDelayChange] = useState(5000);
-  const [statusChange, setStatusChange] = useState('418');
-  const [delay, setDelay] = useState('');
-  const [status, setStatus] = useState('');
+  const [statusChange, setStatusChange] = useState(
+    localStorage.getItem("catStatus") || "418"
+  );
+  const [delay, setDelay] = useState("");
+  const [status, setStatus] = useState("");
 
+  useEffect(() => {
+    localStorage.setItem("catStatus", statusChange);
+  }, [statusChange]);
+
+  useEffect(() => {
+    if (statusChange === "") {
+      alert("Please enter a status code");
+      setStatusChange(404);
+    } else if (VALID_STATUS_CODES.indexOf(parseInt(statusChange)) === -1) {
+      alert(
+        `Code ${statusChange} might exist, but it is not a proper Cat Status code.`
+      );
+      setStatusChange(404);
+    }
+  }, [statusChange]);
+
+  useEffect(() => {
+    const colorInterval = setInterval(
+      () => setColorIdx((prevIdx) => (prevIdx + 1) % COLORS.length),
+      delayChange
+    );
+    return () => {
+      clearInterval(colorInterval);
+    };
+  }, [delayChange]);
+
+  useEffect(() => {
+    const statusTimeout = setTimeout(() => {
+      const status = localStorage.getItem("catStatus");
+      if (status === statusChange) setStatusChange(418)
+    }, 600000);
+    return () => {
+      clearTimeout(statusTimeout);
+    };
+  }, [statusChange]);
 
   const handleDelaySubmit = (e) => {
     e.preventDefault();
 
     if (delay < 1 || delay > 10) {
-      alert('Please enter a delay from 1 through 10!');
+      alert("Please enter a delay from 1 through 10!");
       return;
     }
 
     setDelayChange(Number(delay) * 1000);
-    setDelay('');
+    setDelay("");
   };
 
   const handleStatusSubmit = (e) => {
     e.preventDefault();
 
-    if (status === '') {
-      alert('Please Enter A Code');
-      setStatusChange(404);
-      return;
-    }
+    // if (status === "") {
+    //   alert("Please Enter A Code");
+    //   setStatusChange(404);
+    //   return;
+    // }
 
-    if (!VALID_STATUS_CODES.includes(Number(status))) {
-      alert(
-        `Code ${status} might exist, but it is not a proper Cat Status code.`
-      );
-      setStatusChange(404);
-      return;
-    }
+    // if (!VALID_STATUS_CODES.includes(Number(status))) {
+    //   alert(
+    //     `Code ${status} might exist, but it is not a proper Cat Status code.`
+    //   );
+    //   setStatusChange(404);
+    //   return;
+    // }
 
     setStatusChange(status);
-    setStatus('');
+    setStatus("");
   };
 
   return (
@@ -51,16 +88,13 @@ const Cat = () => {
       className="cat-container"
       style={{
         backgroundColor: COLORS[colorIdx],
-        transition: 'background-color 1s',
+        transition: "background-color 1s",
       }}
     >
       <h1>Cat Status</h1>
-      <button onClick={() => history.push('/')}>Home</button>
+      <button onClick={() => history.push("/")}>Home</button>
       <div className="image-container">
-        <img
-          src={`https://http.cat/${statusChange}`}
-          alt="404"
-        />
+        <img src={`https://http.cat/${statusChange}`} alt="404" />
       </div>
       <form onSubmit={handleDelaySubmit}>
         <label htmlFor="dStatus">
@@ -68,7 +102,7 @@ const Cat = () => {
             type="number"
             id="dStatus"
             onChange={(e) => {
-              setDelay(e.target.value)
+              setDelay(e.target.value);
             }}
             placeholder="delay in seconds"
             value={delay}
